abacd07219150c6970d6dea0783dc2658fd2db49
dennis benavides <denizponce16@gmail.com>
2023-08-14 15:27:01 -0500

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import {Header} from './components/Header';
+import { FormularioTareas } from './components/FormularioTareas';
+import { ListaTareas } from './components/ListaTareas';
 
 function App() {
+  // Obtenemos las tareas guardadas de localstorage
+  const tareasGuardadas = 
+  localStorage.getItem('tareas') ? JSON.parse(localStorage.getItem('tareas')) : []
+
+  //Establecemos el estado de las tareas
+  const [tareas, setTareas] = useState(tareasGuardadas);
+
+  // Guardando el estado dentro del localstorage
+  useEffect(() => {
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+  }, [tareas])
+
+  //Accedemos a localStorage y comprobamos si mostrarCompletadas es null
+  let configMostrarCompletadas = '';
+  if(localStorage.getItem('mostrarCompletadas') === null){
+    configMostrarCompletadas = true;
+  } else {
+    configMostrarCompletadas = localStorage.getItem('mostrarCompletadas') ==='true';
+  }
+
+  // El estado de mostrarCompletadas
+  const [mostrarCompletadas, cambiarMostrarCompletadas] = useState(configMostrarCompletadas);
+
+  useEffect(() => {
+    localStorage.setItem('mostrarCompletadas', mostrarCompletadas.toString());
+  }, [mostrarCompletadas]);
+
   return (
     <div className='contenedor'>
-      <Header />      
+      <Header 
+        mostrarCompletadas={mostrarCompletadas}  
+        cambiarMostrarCompletadas={cambiarMostrarCompletadas} 
+      />    
+      <FormularioTareas tareas={tareas} setTareas={setTareas} /> 
+      <ListaTareas 
+        tareas={tareas} 
+        setTareas={setTareas} 
+        mostrarCompletadas={mostrarCompletadas}
+      /> 
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEyeSlash} from '@fortawesome/free-solid-svg-icons'
+import { faEyeSlash, faEye } from '@fortawesome/free-solid-svg-icons'
 
 
-export const Header = () => {
+export const Header = ({mostrarCompletadas, cambiarMostrarCompletadas}) => {
   return (
     <header className='header'>
         <h1 className='header__titulo'>Lista de tareas</h1>
-        <button className='header__boton'>
+        {mostrarCompletadas ?
+          <button 
+            className='header__boton'
+            onClick={() => cambiarMostrarCompletadas(!mostrarCompletadas)}
+          >
             No mostrar completadas
             <FontAwesomeIcon icon={faEyeSlash} className='header__icono-boton'/>
-        </button>
+          </button>
+        : 
+          <button 
+            className='header__boton'
+            onClick={() => cambiarMostrarCompletadas(!mostrarCompletadas)}
+          >
+            Mostrar completadas
+            <FontAwesomeIcon icon={faEye} className='header__icono-boton'/>
+          </button>
+        }
+        
     </header>
   )
 }
